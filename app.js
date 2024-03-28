b7a6f03e004191dd68a83d6f87a10fb9928a96de
Mostafa Elshamy <melhosseiny@users.noreply.github.com>
2024-03-28 17:59:38 +0200

    deploy to vercel and google cloud

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ const PORT = process.env.PORT || 5000;
 let app = express();
 let server = http.Server(app);
 
-app.use(express.static('dist'));
-app.use(express.static('worker'));
-app.use(express.static('media'));
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'worker')));
+app.use(express.static(path.join(__dirname, 'media')));
 
 app.get('*', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 server.listen(PORT);
+
+export default app;
